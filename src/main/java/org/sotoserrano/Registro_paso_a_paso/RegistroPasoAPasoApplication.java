c94546eb7a812bfc50b7bdb40012e3152eb3a5ce
Estrella <da1d1aestrella@gmail.com>
2022-03-17 13:24:37 +0100

    Faltan POJOS

---
+++ src/main/java/org/sotoserrano/Registro_paso_a_paso/RegistroPasoAPasoApplication.java
@@ -0,0 +1,20 @@
+package org.sotoserrano.Registro_paso_a_paso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author alumno
+ *
+ */
+@SpringBootApplication
+public class RegistroPasoAPasoApplication {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(RegistroPasoAPasoApplication.class, args);
+	}
+
+}
